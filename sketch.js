3406785510b4080077f9ac2cab8b564d9bc84a34
Quinton Ashley <qashto@gmail.com>
2026-02-20 10:07:28 -0500

    1.0.0

---
+++ sketch.js
@@ -0,0 +1,28 @@
+await Canvas();
+world.gravity.y = 10;
+
+let ball = new Sprite();
+ball.x = -200;
+ball.y = -200;
+ball.diameter = 50;
+
+let groundA = new Sprite();
+groundA.x = -120;
+groundA.width = 200;
+groundA.rotation = 20;
+groundA.physics = STATIC;
+
+let groundB = new Sprite();
+groundB.x = 120;
+groundB.width = 200;
+groundB.rotation = -20;
+groundB.physics = STATIC;
+
+q5.update = function () {
+	background('skyblue');
+
+	textAlign(CENTER);
+	text('click to jump!', 0, -100);
+
+	if (mouse.presses()) ball.vel.y = -5;
+};
